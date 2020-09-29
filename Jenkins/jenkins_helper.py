0a93f6fbd02e89029bb8dd8d867dfd632e6018a1
Wandy <wandy1208@gmail.com>
2020-09-29 20:38:02 +0800

    Update jenkins_helper.py

--- Jenkins/jenkins_helper.py
+++ Jenkins/jenkins_helper.py
@@ -11,8 +11,8 @@ class JRunner:
     def build_job(self,job_name, job_params: dict):
         self.server.build_job(job_name, job_params)
     
-    def build_info(self, build_number):
-        # last_build_number = self.server.get_job_info('TMS_Execution_Task')['lastCompletedBuild']['number']
+    def build_info(self, build_number, job_name):
+        # last_build_number = self.server.get_job_info('job_name')['lastCompletedBuild']['number']
         return self.server.get_build_info('TMS_Execution_Task', build_number)
 
     def get_last_completed_build_number(self, job_name):
@@ -22,7 +22,6 @@ class JRunner:
 if __name__ == "__main__":
     pass
     # jr = JRunner("", "", "")
-    # jr.build_job("TMS_Execution_Task", {"project": "oms", "testcases": '["testcases/Smoke","testcases/cart","testcases/Smoke","testcases/cart"]'})
     # number = jr.get_last_completed_build_number()
     # jr.build_info()
     # time.sleep(5)
